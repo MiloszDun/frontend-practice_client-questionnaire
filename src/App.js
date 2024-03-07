4c32de631bff4b092908a44ed6eaca09793a8ac5
Milosz Xavier Dunikowski <milosz@dunikowski.co>
2024-03-07 17:33:48 +0100

    Install MUI. Last commit before refactoring code to implement MUI

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import Form from "./components/Form";
 import SubmissionsList from "./components/SubmissionsList";
 import { SubmissionsProvider } from "./data/SubmissionContext";
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
 
 function App() {
   return (
